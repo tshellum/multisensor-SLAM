fa38f692029246ae39e44e050c9540c6844a1e8d
Martin Eek Gerhardsen <martiege@stud.ntnu.no>
2021-03-02 10:44:17 +0100

    move some of backend to cpp

--- src/backend/src/backend/backend.cpp
+++ src/backend/src/backend/backend.cpp
@@ -5,15 +5,15 @@ namespace backend
 {
 
 Backend::Backend() 
-  : pose_id_(0),
-  pose_(gtsam::Pose3::identity()),
-  buffer_size_(1000),
-  optimize_timer_(nh_.createTimer(ros::Duration(5), &Backend::callback, this)), 
-  updated_(true),
-  graph_filename_("graph.dot"), result_path_(ros::package::getPath("backend") + "/../../results/")
-{
-  world_pose_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/backend/pose_world", buffer_size_);
-};
+  : pose_id_(0), time_prev_pose_relative_(ros::Time(0.0)),
+    updated_(false), nav_status_(false), imu_status_(std::make_pair(false, false)),
+    association_threshold_(0.01),
+    pose_(gtsam::Pose3::identity()),
+    graph_filename_("graph.dot"), result_path_(ros::package::getPath("backend") + "/../../results/"),
+    buffer_size_(1000),
+    optimize_timer_(nh_.createTimer(ros::Duration(0.1), &Backend::callback, this)),
+    world_pose_pub_(nh_.advertise<geometry_msgs::PoseStamped>("/backend/pose_world", buffer_size_)) 
+  {}
 
 Backend::~Backend() 
 {
@@ -23,8 +23,10 @@ Backend::~Backend()
 
 void Backend::callback(const ros::TimerEvent& event)
 {
-  if (new_values_.size())
+  if (updated_ || (imu_status_.first && imu_status_.second))
   {
+    ros::Time tic = ros::Time::now();
+    
     // new_values_.print();
 
     isam2_.update(new_factors_, new_values_);
@@ -34,12 +36,19 @@ void Backend::callback(const ros::TimerEvent& event)
     gtsam::Values current_estimate = isam2_.calculateBestEstimate();
     pose_ = current_estimate.at<gtsam::Pose3>(gtsam::symbol_shorthand::X(pose_id_));
 
-    // pose_.print();
+    pose_.print();
+    world_pose_pub_.publish(generateMsg());
 
+
+    // Reset parameters
     new_factors_.resize(0);
     new_values_.clear();
+    stamped_pose_ids_.clear(); // Alternatively [begin, previous relative pose]
+    updated_ = false;
+    imu_status_.second = false;
 
-    world_pose_pub_.publish(generateMsg());
+    ros::Time toc = ros::Time::now();
+    ROS_INFO_STREAM("Time per iteration: " <<  (toc - tic) << "\n");
   }
 }
 
@@ -57,7 +66,7 @@ geometry_msgs::PoseStamped Backend::generateMsg()
 }
 
 
-int Backend::searchAssociatedPose(ros::Time pose_stamp)
+std::pair<int, bool> Backend::searchAssociatedPose(ros::Time pose_stamp, ros::Time prev_pose_stamp)
 {    
   std::map<ros::Time, int>::iterator stamped_pose_id = stamped_pose_ids_.begin();
   std::map<ros::Time, int>::iterator prev = stamped_pose_ids_.begin();
@@ -73,21 +82,27 @@ int Backend::searchAssociatedPose(ros::Time pose_stamp)
   double prev_diff = std::abs((pose_stamp - prev->first).toSec());
   double diff = std::abs((pose_stamp - stamped_pose_id->first).toSec());
 
-  if ( (prev_diff < 0.01) || (diff < 0.01) ) // is associated
+  // Check if associated relative pose. If associated, check if previous or next is closest
+  if ( updated_
+    && ((prev_diff < association_threshold_) 
+    || (diff < association_threshold_)) ) 
   {
-    if (diff < prev_diff)
-      return stamped_pose_id->second;
-    else
-      return prev->second;
+    if ( (diff > prev_diff) && (prev->first > prev_pose_stamp)) // Ensure that previous association isn't the actual previous pose 
+    {
+      stamped_pose_ids_[pose_stamp] = prev->second; 
+      return std::make_pair(prev->second, true);            // Prev is associated
+    }
+
+    if (stamped_pose_id->first > prev_pose_stamp)
+    {
+      stamped_pose_ids_[pose_stamp] = stamped_pose_id->second; 
+      return std::make_pair(stamped_pose_id->second, true); // Next is associated
+    }
   }
 
-  if (stamped_pose_id != stamped_pose_ids_.end())
-    return stamped_pose_id->second;
-  else
-  {
-    stamped_pose_ids_[pose_stamp] = ++pose_id_;
-    return pose_id_;
-  }
+  // Not associated pose --> Increase pose_id. 
+  stamped_pose_ids_[pose_stamp] = ++pose_id_; 
+  return std::make_pair(pose_id_, false);                   // New pose at end
 }
 
 
